c0849ea6053371262daa0eaf91639dc02f23d575
David Fry <david.fry@modusbox.com>
2021-09-01 14:53:59 +0100

    latest fix

--- terraform/k8s-setup/addons/shared_key_locals.tf
+++ terraform/k8s-setup/addons/shared_key_locals.tf
@@ -1,7 +1,10 @@
-
 locals {
-  switch_pem = map("switch.pem", data.terraform_remote_state.k8s-base.outputs.switch_jws_key)
-  non_resp   = map("noresponsepayeefsp.pem", tls_private_key.simulators["payeefsp"].public_key_pem)
+  switch_pem = tomap({
+                  "switch.pem" = data.terraform_remote_state.k8s-base.outputs.switch_jws_key
+                })
+  non_resp   = tomap({
+                  "noresponsepayeefsp.pem" = tls_private_key.simulators["payeefsp"].public_key_pem
+                })
   sim_pem    = { for value in toset(var.simulator_names) : "${value}.pem" => tls_private_key.simulators[value].public_key_pem }
   jws_pub    = merge(local.switch_pem, local.non_resp, local.sim_pem)
 }
