bc30a00e3f62be3d4a0ebc183d087e28f66d52b5
David Fry <david.fry@modusbox.com>
2022-03-04 13:45:51 +0000

    rm ref to aws provideer

--- terraform/modules/wso2-init/variables.tf
+++ terraform/modules/wso2-init/variables.tf
@@ -3,11 +3,6 @@ variable "kubeconfig" {
   type = string
 }
 
-variable "region" {
-  description = "AWS Region used"
-  type = string
-}
-
 variable "environment" {
   description = "Environment Name"
   type = string
@@ -27,28 +22,6 @@ variable "db_root_password" {
   description = "Password for DB 'root' user"
   type = string
 }
-
-variable "helm_efs_provisioner_version" {
-  description = "Chart version for the efs provisioner"
-  type = string
-}
-
-variable "efs_subnet_ids" {
-  description = "Subnet IDs to use with EFS"
-  type = list(string)
-}
-
-variable "efs_security_groups"  {
-  description = "List of security group IDs to apply to the EFS. Must contain at least the sg id for the environment"
-  type = list(string)
-}
-
-variable "efs_storage_class_name" {
-  description = "storage class name for efs"
-  type = string
-  default = "efs"
-}
-
 variable "namespace" {
   description = "namespace to deploy to"
   type = string
